39e1f3268af77e17d1118120559e9cd18d235f87
Michelle Harley <harleyma1851@gmail.com>
2022-09-30 16:30:58 -0500

    adjusted API
    ended up not being the issue, reverted back to original.

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ function displayWeatherCondition(response) {
 
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayWeatherCondition);
